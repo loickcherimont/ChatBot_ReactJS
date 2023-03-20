5864db8a0a790b77c1ca857cdd011f562ccf140e
Loick <loickcherimont@gmail.com>
2023-03-20 23:16:57 +0100

    Temp: Detect if use asks time

--- src/bot/MessageParser.jsx
+++ src/bot/MessageParser.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { createClientMessage } from "react-chatbot-kit";
+// import { createClientMessage } from "react-chatbot-kit";
 
 const MessageParser = ( { children, actions }) => {
     const parse = (message) => {
+        // Response to user's greetings
         let regex = new RegExp("hello", "i");
         if(regex.test(message)) {
             actions.handleHello();
         }
 
+        // Show user a random picture standing for a dog
         regex = new RegExp("dog", "i");
         if (regex.test(message)) {
-            actions.handleDog()
+            actions.handleDog();
+        }
+
+        regex = new RegExp("time", "i");
+        if(regex.test(message)) {
+            actions.handleTime();
         }
     };
 
