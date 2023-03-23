f823dec62777ec8abe17088d0d3de85d7dd4ffcc
LoickDev97438 <loickcherimont@gmail.com>
2023-03-23 22:52:06 +0100

    Replace ReactDOM with createRoot

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
 
 // Display App in HTML
-ReactDOM
-  .createRoot(document.getElementById("root"))
+root.createRoot(document.getElementById("root"))
   .render(<App />);
