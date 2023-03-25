bb40b459e767728ba5445227019425776572692d
LoickDev97438 <loickcherimont@gmail.com>
2023-03-25 22:30:06 +0100

    Add React Icons library and style the chatbot

---
+++ src/components/BotAvatar/BotAvatar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./BotAvatar.css";
+// Use react icons library
+import { FaRobot } from "react-icons/fa"; 
+
+const BotAvatar = () => {
+    return (
+        <div className="lowbot-avatar">
+            <FaRobot />
+        </div>
+    )
+}
+
+export default BotAvatar;
