14eabb5288d684684603f97472279cbec12b955f
LoickDev97438 <loickcherimont@gmail.com>
2023-03-25 22:29:12 +0100

    Add stylesheet to Options

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Options.css"
+
+const Options = (props) => {
+    // Possibilities for ChatBot
+    const options = [
+        {
+            text: "Who am I?",
+            handler: props.actionProvider.handleDescribeBot,
+            id: 1,
+        },
+        {
+            text: "User location",
+            handler: props.actionProvider.handleUserLocation,
+            id: 2,
+        }
+
+    ];
+
+    const buttonsMarkup = options.map((option) => (
+        <button key={option.id} onClick={option.handler} className="option-button">
+            {option.text}
+        </button>
+    ));
+
+    return <div className="options-container">
+        {buttonsMarkup}
+    </div>;
+}
+
+export default Options;
